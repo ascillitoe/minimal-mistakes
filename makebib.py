ddf15b932cad21afddc140b382b37074dff99475
Ashley Scillitoe <ashleyscillitoe@googlemail.com>
2021-01-18 14:23:54 +0000

    More papers and images, started makebib scripts

---
+++ makebib.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import yaml
+import os
+
+paperdir = 'assets/papers/'
+
+#get .bib for files with arXiv identifier
+with open(r'_data/publist.yml') as file:
+    pubs = yaml.load(file, Loader=yaml.FullLoader)
+
+    for pub in pubs:
+        for k,v in pub.items():
+            if (k=='url'):
+                url=v
+            if (k=='arxiv'):
+                print('working on ArXiv file ' + url)
+                os.system("./scripts/arxiv2bib.py " + str(v) + " > " + paperdir + url +".txt")
+                infile = open(os.pathjoin(paperdir, + url +".txt"),'r')
+                tmpfile = os.path.join(paperdir,"tmp.bib")
+                newopen = open(tmpfile,'w')
+                for line in infile:
+                    if 'File' not in line and 'Eprint' not in line and 'PrimaryClass' not in line:
+                        newopen.write(line)
+                newopen.close()
+                os.system("mv " + tmpfile + os.path.join(paperdir,url + ".txt"))
+
+##get .bib for files with DOI 
+#with open(r'_data/publist.yml') as file:
+#    pubs = yaml.load(file, Loader=yaml.FullLoader)
+#    for pub in pubs:
+#        for k,v in pub.items():
+#            if (k=='url'):
+#                url=v
+#            if (k=='doi'):
+#                print('working on DOI file ' + url)
+#                os.system("./scripts/doi2bib.py " + str(v) + " > papers/" + url +".txt")
+#                #remove empty lines that doi2bib gives
+#                open("papers/tmp.bib",'w').write(
+#                    ''.join(
+#                        l for l in open("papers/" + url + ".txt") if l.strip()))
+#                # os.system("mv papers/tmp.bib papers/" + url + ".txt")
+#                infile = open("papers/tmp.bib",'r')
+#                newopen = open("papers/tmp2.bib",'w')
+#                for line in infile:
+#                    if 'url' not in line and 'publisher' not in line:
+#                        newopen.write(line)
+#                newopen.close()
+#                os.system("mv papers/tmp2.bib papers/" + url + ".txt")
+#                os.system("rm -f papers/tmp*.bib")
